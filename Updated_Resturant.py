26e26aee1ac96a531b9e8b3ab7db2a9e7121ede5
Chalen33 <98927158+Chalen33@users.noreply.github.com>
2022-09-29 09:39:59 -0400

    Restaurant.py with added while loop

--- Updated_Resturant.py
+++ Updated_Resturant.py
@@ -2,34 +2,43 @@
 #Chalen Bobish
 #Resturant
 
-vegetarian=False
-vegan=False
-gluten=False
+another_search = 'yes'
+while another_search == 'yes':
+    
+    vegetarian=False
+    vegan=False
+    gluten=False
 
-response=input("Is anyone in your party a vegetarian?\n")
-if response == 'yes':
-    vegetarian= True
+    response=input("Is anyone in your party a vegetarian?\n")
+    if response == 'yes':
+        vegetarian= True
 
-response=input("Is anyone in your party a Vegan?\n")
-if response == 'yes':
-    vegan= True
+    response=input("Is anyone in your party a Vegan?\n")
+    if response == 'yes':
+        vegan= True
 
-response=input("Is anyone in your party gluten-free?\n")
-if response == 'yes':
-    gluten= True
+    response=input("Is anyone in your party gluten-free?\n")
+    if response == 'yes':
+        gluten= True
 
-if vegan:
-    print("Here are your restaurant choices:"+
+    if vegan:
+        print("Here are your restaurant choices:"+
           "\nCorner Café\nThe Chef's Kitchen")
 
-elif gluten:
-    print("Here are your restaurant choices:"+
+    elif gluten:
+        print("Here are your restaurant choices:"+
           "\nMain Street Pizza Company\nCorner Café\nThe Chef's Kitchen")
 
-elif vegetarian:
-    print("Here are your restaurant choices:"+
+    elif vegetarian:
+        print("Here are your restaurant choices:"+
           "\nMain Street Pizza Company\nCorner Café\nMama's Fine Italian\nThe Chef's Kitchen")
     
-else:
-    print('Here are your restaurant choices:'+
+    else:
+        print('Here are your restaurant choices:'+
           "\nJoe's Gourmet Burgers\nMain Street Pizza Company\nCorner Café\nMama's Fine Italian\nThe Chef's Kitchen")
+
+    another_search= input("Enter 'yes' if you like to do another restaurant search (enter 'no' to end):")
+
+    
+        
+        
