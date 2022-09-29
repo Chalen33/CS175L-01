7201f780f0a43b8b607a8da8c47df9bfe01330e9
Chalen33 <98927158+Chalen33@users.noreply.github.com>
2022-09-29 08:55:43 -0400

    Resturant.py

---
+++ Updated_Resturant.py
@@ -0,0 +1,35 @@
+#CS175L
+#Chalen Bobish
+#Resturant
+
+vegetarian=False
+vegan=False
+gluten=False
+
+response=input("Is anyone in your party a vegetarian?\n")
+if response == 'yes':
+    vegetarian= True
+
+response=input("Is anyone in your party a Vegan?\n")
+if response == 'yes':
+    vegan= True
+
+response=input("Is anyone in your party gluten-free?\n")
+if response == 'yes':
+    gluten= True
+
+if vegan:
+    print("Here are your restaurant choices:"+
+          "\nCorner Café\nThe Chef's Kitchen")
+
+elif gluten:
+    print("Here are your restaurant choices:"+
+          "\nMain Street Pizza Company\nCorner Café\nThe Chef's Kitchen")
+
+elif vegetarian:
+    print("Here are your restaurant choices:"+
+          "\nMain Street Pizza Company\nCorner Café\nMama's Fine Italian\nThe Chef's Kitchen")
+    
+else:
+    print('Here are your restaurant choices:'+
+          "\nJoe's Gourmet Burgers\nMain Street Pizza Company\nCorner Café\nMama's Fine Italian\nThe Chef's Kitchen")
