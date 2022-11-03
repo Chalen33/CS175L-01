0984c7725cbcb6df126c1a1720e7c5c39e2b2f72
Chalen33 <98927158+Chalen33@users.noreply.github.com>
2022-11-03 16:58:14 -0400

    AverageFromInput.py
    Version 2 with Exception Handling

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -1,15 +1,24 @@
 def main():
     counter=0
     total=0
-    avgFile= open('numbers.txt','r')
-    for num in avgFile:
-        n=float(num)
-        total+=n
-        counter= counter+1
-        print('I read in ', counter, 'number(s) Current number is: ', f'{n:>8.2f}','Total is: ', f'{total:>8.2f}')
-    avg= total/counter
-    print('Average:', avg)
-    avgFile.close()
-main()
-    
+    try:
+        avgFile= open('numbers.txt','r')
+        for num in avgFile:
+            try:
+                n=float(num)
+            except ValueError:
+                num=num.strip('\n')
+                print('One of your numbers is not an int or float')
+            else:
+                total+=n
+                counter= counter+1
+                print('I read in ', counter, 'number(s) Current number is: ', f'{n:>8.2f}','Total is: ', f'{total:>8.2f}')
+        avg= total/counter
+        print('Average:', avg)
+        avgFile.close()
+    except IOError:
+        print('File not found')
+if __name__ == '__main__':
+    main()
+     
         
